7f2800961254665eed6d2c11b00c9edd998cdaa1
zueunjoeng <judms0314@gmail.com>
2024-06-18 11:24:11 +0900

    array로 오브젝트 접근하기-소메뉴

--- ajax/js/ajax.js
+++ ajax/js/ajax.js
@@ -10,13 +10,22 @@ $(document).ready(function(){
 
             let gnbli="";
             data["menu"].forEach(function(ele, idx){
-                //순서대로 순회하면서 실행
+                // 순서대로 순회하면서 실행
                 console.log(`${idx}번째 데이터는 ${ele}`)
-                gnbli +=`<li>${ele["category"]}</li>`
-
+                gnbli +=`<li>${ele["category"]}`;
+                gnbli +=`<ul>`;
+                ele["subcategories"].forEach(function(eele, iidx){
+                    gnbli +=`<li>${eele["name"]}</li>`;
+                })
+                gnbli +=`</ul>`;
+                gnbli +=`</li>`
+                
             });
+
             $(".gnb").html(gnbli)
+
         },
+
         
         error : function(a, b, c){
             console.log(a, b, c);
