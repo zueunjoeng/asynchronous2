2de21b61841eda989faeeb86d64921f7205edd23
zueunjoeng <judms0314@gmail.com>
2024-06-18 10:57:04 +0900

    json데이터를 기반, foreach문으로 자바스크립트 console확인 가능

--- ajax/js/ajax.js
+++ ajax/js/ajax.js
@@ -6,7 +6,16 @@ $(document).ready(function(){
         success : function(data){
             console.log(data, typeof data);
             //JSON.parse(data) -> 자바스크립트가 쓸 수 있게 object
+            // $("#here").html(data["menu"][0]["category"]) //data["menu"][0]에서 foreach발생
+
+            let gnbli="";
+            data["menu"].forEach(function(ele, idx){
+                //순서대로 순회하면서 실행
+                console.log(`${idx}번째 데이터는 ${ele}`)
+            });
+            $(".gnb").html()
         },
+        
         error : function(a, b, c){
             console.log(a, b, c);
         }
